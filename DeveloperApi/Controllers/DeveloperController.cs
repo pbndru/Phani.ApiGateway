009c2cf804da3a3511b282f43b08644b1d12e25b
phani bandaru <pbndru@gmail.com>
2022-02-06 22:18:42 +0000

    Add project files.

---
+++ DeveloperApi/Controllers/DeveloperController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DeveloperApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly ILogger<DeveloperController> _logger;
+
+        public DeveloperController(ILogger<DeveloperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "C#", "SqlServer", "Nodejs"};
+        }
+    }
+}
---
+++ TesterApi/Controllers/TesterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesterApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TesterController : ControllerBase
+    {
+        private readonly ILogger<TesterController> _logger;
+
+        public TesterController(ILogger<TesterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Manual", "Automation", "Performance" };
+        }
+    }
+}
